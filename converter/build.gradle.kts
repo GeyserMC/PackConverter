64d1290d85578543ea8e22ca7e3c437f979ee71b
onebeastchris <github@onechris.mozmail.com>
2024-03-12 21:37:24 +0100

    make these implementation

--- converter/build.gradle.kts
+++ converter/build.gradle.kts
@@ -12,8 +12,8 @@ sourceSets {
 
 dependencies {
     api(project(":pack-schema-api"))
-    compileOnly("com.google.code.gson:gson:2.10.1")
-    compileOnly("commons-io:commons-io:2.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("commons-io:commons-io:2.11.0")
     implementation("com.twelvemonkeys.imageio:imageio-tga:3.9.4")
     implementation("com.nukkitx.fastutil:fastutil-int-object-maps:8.5.3")
     api("net.kyori:adventure-api:4.14.0")
