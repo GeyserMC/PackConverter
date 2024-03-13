f3888495b0596181cd091cb831cd845a53d375b5
onebeastchris <github@onechris.mozmail.com>
2024-03-13 16:00:59 +0100

    move version/group setting to gradle.properties, publish all necessary artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     id("pack.base-conventions")
 }
 
-subprojects{
+subprojects {
     plugins.apply("pack.base-conventions")
     plugins.apply("pack.publish-conventions")
-    group = "org.geysermc.pack"
-    version = "3.1-SNAPSHOT"
 
     java.sourceCompatibility = JavaVersion.VERSION_17
     java.targetCompatibility = JavaVersion.VERSION_17
