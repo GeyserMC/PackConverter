c380c8e53228acb3e14e531b8e1ad3267b4360ec
SleepyPop <81194303+SleepyPopp@users.noreply.github.com>
2026-03-06 16:24:27 -0500

    Set Java source and target compatibility to VERSION_21

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ subprojects {
     plugins.apply("pack.base-conventions")
     plugins.apply("pack.publish-conventions")
 
-    java.sourceCompatibility = JavaVersion.VERSION_17
-    java.targetCompatibility = JavaVersion.VERSION_17
+    java.sourceCompatibility = JavaVersion.VERSION_21
+    java.targetCompatibility = JavaVersion.VERSION_21
 }
