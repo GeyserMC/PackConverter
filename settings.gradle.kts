4267f450b753a9dd635ef4980ca44c3b2a0b2fac
SleepyPop <81194303+SleepyPopp@users.noreply.github.com>
2026-05-11 14:56:12 -0400

    Remove JitPack and use NexoMC's maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,15 +25,7 @@ dependencyResolutionManagement {
         maven("https://repo.opencollab.dev/main")
         maven("https://repo.opencollab.dev/maven-snapshots")
 
-        // Java pack library
-        // maven("https://repo.unnamed.team/repository/unnamed-public/")
-
-        // This repo is dead.
-//        maven { // We need a snapshot version of creative for now
-//            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-//        }
-
-        // But we still need creative... jitpack it is
-        maven("https://jitpack.io")
+        // creative
+        maven("https://repo.nexomc.com/releases/")
     }
 }
