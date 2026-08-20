c02b56a7f1d62f2d4f136b63b256759e90e8578f
nguyenphuck2468-tech <nguyenphuck2468@gmail.com>
2026-08-20 20:01:23 +0700

    build: target Java 25 for Minecraft 26.2 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ subprojects {
     plugins.apply("pack.base-conventions")
     plugins.apply("pack.publish-conventions")
 
-    java.sourceCompatibility = JavaVersion.VERSION_21
-    java.targetCompatibility = JavaVersion.VERSION_21
+    java.sourceCompatibility = JavaVersion.VERSION_25
+    java.targetCompatibility = JavaVersion.VERSION_25
 }
