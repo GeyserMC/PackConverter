487526cbc66517053eb784efdba705190f69d7d5
nguyenphuck2468-tech <nguyenphuck2468@gmail.com>
2026-08-20 21:24:08 +0700

    build: upgrade Shadow plugin for Gradle 9.7 compatibility

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.johnrengelman:shadow:8.1.1")
+    implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:9.6.1")
 }
 
 repositories {
