71d3d7b8a97c0129762bcda6fde9dc051957ca93
rtm516 <ryantmilner@hotmail.co.uk>
2026-08-30 14:47:25 +0100

    Fix texture output using the java root instead of the mapping root

--- converter/src/main/java/org/geysermc/pack/converter/type/texture/TextureConverter.java
+++ converter/src/main/java/org/geysermc/pack/converter/type/texture/TextureConverter.java
@@ -112,13 +112,17 @@ public class TextureConverter implements AssetExtractor<Texture>, AssetConverter
         String input = texture.key().value();
         String relativePath = input.replaceAll("\\.png$", "");
 
-        String rootPath = relativePath.substring(0, relativePath.indexOf('/'));
-        String bedrockRoot = DIRECTORY_LOCATIONS.getOrDefault(rootPath, rootPath);
-
         List<String> mapping = mappings.map(relativePath);
         List<String> transformedOutputs = new ArrayList<>();
         for (String item : mapping) {
-            transformedOutputs.add(bedrockRoot + item.substring(item.indexOf('/')) + ".png");
+            int rootEnd = item.indexOf('/');
+            if (rootEnd == -1) {
+                context.warn("Mapping " + item + " for texture " + input + " has no root directory, skipping!");
+                continue;
+            }
+
+            String rootPath = item.substring(0, rootEnd);
+            transformedOutputs.add(DIRECTORY_LOCATIONS.getOrDefault(rootPath, rootPath) + item.substring(rootEnd) + ".png");
         }
 
         transformed.output(transformedOutputs);
